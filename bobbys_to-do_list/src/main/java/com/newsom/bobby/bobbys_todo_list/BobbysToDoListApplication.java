f69c43f3417d69f3f7d92aee6180564aa6614789
Bobby Newsom <bobby.newsom@gmail.com>
2024-05-07 16:22:48 -0500

    Initial commit

---
+++ bobbys_to-do_list/src/main/java/com/newsom/bobby/bobbys_todo_list/BobbysToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.newsom.bobby.bobbys_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobbysToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobbysToDoListApplication.class, args);
+	}
+
+}
---
+++ bobbys_to-do_list/src/test/java/com/newsom/bobby/bobbys_todo_list/BobbysToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsom.bobby.bobbys_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobbysToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
